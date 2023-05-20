85aaf975df99d3c8014dd282db9821bda558e336
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-20 16:19:27 -0400

    crear nav-mobile y darle movimiento

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
 const icon_hamburguer = document.getElementById('icon-nav');
 
 icon_hamburguer.addEventListener('click', function() {
+    const mobileMenu = document.querySelector('.mobile-menu');
 
-    if (icon_hamburguer.getAttribute('name') == 'reorder-four-outline') {
+    if (mobileMenu.style.left == '-260px') {
         icon_hamburguer.setAttribute('name', 'close-outline');
+        mobileMenu.style.left = '0px';
     }else{
         icon_hamburguer.setAttribute('name', 'reorder-four-outline');
+        mobileMenu.style.left = '-260px';
     }
 });
